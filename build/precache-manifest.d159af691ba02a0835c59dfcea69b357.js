21863eed9198767df383bfe9f4050584838f8d1a
Sean Stone <smstone@uvm.edu>
2019-02-11 22:56:25 -0500

    include build

---
+++ build/precache-manifest.d159af691ba02a0835c59dfcea69b357.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5aa283184ff75374d2e",
+    "url": "/static/js/main.d5aa2831.chunk.js"
+  },
+  {
+    "revision": "29807171ab1295e4720c",
+    "url": "/static/js/1.29807171.chunk.js"
+  },
+  {
+    "revision": "d5aa283184ff75374d2e",
+    "url": "/static/css/main.6fd5785d.chunk.css"
+  },
+  {
+    "revision": "04209920df5b5e98f9fb3b5e8c93153b",
+    "url": "/index.html"
+  }
+];
